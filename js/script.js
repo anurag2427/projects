d985800ae55d1443ce2325328d3b303e6a0f5727
anurag2427 <anurag424.at@gmail.com>
2018-06-12 10:54:46 +0530

    yes

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function education()
+{
+	 document.getElementById('a').innerHTML='<h2><center>EDUCATION</center></h2><br><ul><li><strong>GRADUATION: Lovely Professional University</strong></li><li>Passing Year: 2019</li><li>Current CGPA: 7.79</li><br><br><li><strong>SCHOOLING</strong></li><li>Intermediate: BOHRA PUBLIC SCHOOL</li><li>Passing Year: 2015</li><li>Board: CBSE</li><li>Percentage: 71.60%</li><br><li>Matriculation: BOHRA PUBLIC SCHOOL</li><li>Passing Year: 2013</li><li>Board: CBSE</li><li>CGPA: 9.40</li></ul>';
+   	 document.getElementById('a').style.height="580px";
+	 document.getElementById('b').style.height="580px";
+} 
+
+function keyProjects()
+{
+	document.getElementById('a').innerHTML='<h2><center>KEY PROJECTS</center></h2><br><ul><li><strong>Pizza Delivery Billig System</strong></li><li>Technology Used: C++<ul><li>Developed a Pizza Delivery Billing System which was successful in selection of Pizza from the menu that was fed to the program with its respective cost. The data is in a tabular form and we can calaculate the total cost price with the discounts available to print the final bill.</li></ul></li><li><strong>Calculator using GUI</strong></li><li>Technology Used: Python<ul><li>Developed calculator using <em>tKinter library<em> of python to add GUI and making every button of a calculator on screen work with their respective function on clicking on them.</li></ul></li><li><strong>Bio-Data Resume Profile Generator</strong></li><li>Technology Used: PHP<ul><li>Created a complete website which displays the complete Bio-data of a person to which his resume was also attached which was in downloadable format. Also displays the details of seminars one has attended, place he has been invited to as a guest and workshop taken. Simply a ready to go website of a lecturer which can display complete detail of him.</li></ul></li><li><strong>Other Projects</strong><ul><li>Created a <em>Color Code RGB Guessing Game</em> using <em>HTML, JavaScript and Bootstrap</em></li><li>Created a <em>Complaint Management System</em> using <em>Laraval</em>(PHP Framework).</li><li>Also created LMS, Portfolio Websites, Image Gallery and To-do List using <em>HTML, CSS, Bootstrap and Javascript</em>.</li></ul></li></ul>';
+	document.getElementById('a').style.height="790px";
+	document.getElementById('b').style.height="790px";
+}
+
+function skills()
+{
+	document.getElementById('a').innerHTML='<h2><center>SKILLS</center></h2><br><ul><li><strong>Programming Languages</strong><ul><li>C and C++</li><li>More: Java 8 & Python</li></ul></li><li>Web Development<ul><li>HTML, CSS, JavaScript<li>PHP<li><li>Also Worked with: jQuery and Bootstrap</li></ul></li><li><strong>Frameworks</strong><ul><li>Angular JS, Paper JS Basics, Laraval Basics</li></ul></li><li><strong>Areas of Interest</strong></li><ul><li>Web Development and Programming</li></ul></ul>';
+	document.getElementById('a').style.height="500px";
+	document.getElementById('b').style.height="500px";
+}
+
+function campAct()
+{
+	document.getElementById('a').innerHTML='<h2><center>CAMPUS ACTIVITIES</center></h2><br><ul><li><strong>Core Member of  Club20(First Sports Club)</strong></li><li><em>Student organization under the aegis of SOC</em></li><li><ul><li>Coordinated and conducted sports events at University level</li><li>Maintaining and managing data regarding the events as a leading data handler of the team</li></ul></li><br><br><li><h2>Extracurricular Activities</h2></li><li>Awarded Young Scientist Award By Narayana at School level in 2010.</li><li>Participated in PRIMS Marathon focused on theme "Run to end AIDS" and the Daily World Marathon on theme "Run for Chandigarh" in 2017-2018.</li><li>NTSC qualified with 50% in 2011.</li></ul>';
+	document.getElementById('a').style.height="520px";
+	document.getElementById('b').style.height="520px";
+}
+
+
+function aboutMe()
+{
+	document.getElementById('a').innerHTML='<h2><center>ABOUT ME</center></h2><br><ul><li>ANURAG TIWARI</li><li>LOVELY PROFESSIONAL UNIVERSITY</li><li>COURSE: BACHELORS OF TECHNOLOGY</li><li>BRANCH: COMPUTER SCIENCE AND ENGINEERING</li><li>REGISTRATION NUMBER: 11505001</li><li>SPECIALIZATION: FULL STACK WEB DEVELOPMENT</li><li>GENDER: MALE</li><li>DATE OF BIRTH: 26/08/1997</li></ul>';
+	document.getElementById('a').style.height="500px";
+	document.getElementById('b').style.height="500px";
+}																										
+
+function contactDetails()
+{
+	document.getElementById('a').innerHTML='<h2><center>CONTACT DETAILS</center></h2><br><ul><li>ANURAG TIWARI</li><li><strong>Address:</strong><ul><li>A/123, <br>Chawla Colony, Ballabhgarh,<br>Faridabad, Haryana-121004</li></ul></li><li><strong>Mobile Number:</strong><ul><li> 9650119744</li><li>8700829528</li></ul></li><li><strong>Email Me @: </strong><ul><li>anurag424.at@gmail.com</li><li>anuragtiwari12349@yahoo.com</li></ul></li></ul>';
+	document.getElementById('a').style.height="500px";
+	document.getElementById('b').style.height="500px";
+}
